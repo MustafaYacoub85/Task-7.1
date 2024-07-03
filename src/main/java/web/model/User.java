8c38e830d45702562d0b3d7be2772492f5f8f28a
MustafaYacoub85 <mustafa.yacoub2013@gmail.com>
2024-07-03 09:53:47 +0300

    Second Commit (Заменил сетторы, гетторы и конструкторы на lombok )

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -7,6 +7,8 @@ import lombok.NonNull;
 import javax.persistence.*;
 
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "user1")
 public class User {
@@ -14,67 +16,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     Long id;
-
+    @NonNull
     @Column(name = "firstname")
     String firstname;
-
+    @NonNull
     @Column(name = "lastname")
     String lastname;
-
+    @NonNull
     @Column(name = "age")
     int age;
-
+    @NonNull
     @Column(name = "email")
     String email;
 
-    public User(String firstname, String lastname, int age, String email) {
-
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-        this.email = email;
-    }
-    public User() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
